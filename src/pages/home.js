f36d590af39a1df4ebcdaf7a9af535281b6db5d3
PAAWSA09LC0914 <Duoc@PAAWSA09LC0914.duoc.acad>
2025-11-04 21:37:23 -0300

    primera clase

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePrincipal(){
+    return(
+        <div className="row">
+            <div className='col-md-12'>
+                <h2>Pagina Principal</h2>
+                <p>ASDASDSADASDSADSADSADSA ASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSA
+                    ASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSA
+                </p>
+            </div>
+        </div>
+    );
+
+}
+
+export default HomePrincipal;
---
+++ src/pages/products.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProductsPrincipal(){
+    return(
+            <div className="row">
+            <div className='col-md-12'>
+                <h2>Productos destacados</h2>
+                <p>ASDASDSADASDSADSADSADSA ASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSA
+                    ASDASDSADASDSADSADSADSAASDASDSADASDSADSADSADSA</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductsPrincipal;
